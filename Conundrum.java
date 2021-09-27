0ef3d8a6894b4a19324c130aa457c2d317ebaf74
alfie <alfiechalmers@yahoo.com>
2021-09-27 20:46:33 +0100

    Seems to work 100%
    May come back later to improve or may be done with this one for good.

--- Conundrum.java
+++ Conundrum.java
@@ -14,47 +14,49 @@ public class Conundrum {
 
 		Scanner scanner = new Scanner(System.in);
 		int lives = 3;
+		int attempts = 0;
 
 		while (lives > 0) {
 			String guess = scanner.nextLine();
 
 			if (guess.equals(originalWord)) {
+				attempts++;
 				System.out.println("Correct!");
+				break;
 			} else {
+				attempts++;
 				lives -= 1;
-				System.out.println("Not this time, you have " + lives + "lives left...");
-			}
-			if (lives == 0) {
-				System.out.println("Better luck next time...");
-				System.exit(1);
-			} else {
-				System.out.println("Well done! You did it on your " + ((lives - 3) * -1) + " guess");
-				//TODO Fix "Well done! You did it on your " + ((lives - 3) * -1) + " guess" Number
-				//TODO Fix well done case (goes straight to else)
-				System.exit(1);
+				System.out.println("Not this time, you have " + lives + " lives left...");
 			}
 		}
+		if (lives == 0) {
+			System.out.println("Better luck next time...");
+			System.exit(1);
+		} else {
+			System.out.println("Well done! You did it on your " + (attempts) + " guess");
+			System.exit(1);
+		}
+		scanner.close();
 
 	}
 
-	
 	public static String getScrambledWord(String word) {
 		ArrayList<String> wordCharacters = new ArrayList<String>(Arrays.asList(word.split("")));
 		Collections.shuffle(wordCharacters);
 		String shuffledWord = String.join("", wordCharacters);
-		
+
 		return shuffledWord;
 	}
-	
+
 	public static String getWord() {
-			FileReader fileReader = new FileReader("./words.txt");
-			ArrayList<String> words = new ArrayList<String>();
-			while (fileReader.hasNext()) {
-				words.add(fileReader.next());
-			}
-			fileReader.close();
-			String word = words.get((int) Math.floor(Math.random() * words.size()));
-			return word;	
+		FileReader fileReader = new FileReader("./words.txt");
+		ArrayList<String> words = new ArrayList<String>();
+		while (fileReader.hasNext()) {
+			words.add(fileReader.next());
+		}
+		fileReader.close();
+		String word = words.get((int) Math.floor(Math.random() * words.size()));
+		return word;
 	}
 
 }
